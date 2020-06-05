08946f958888e67f59dfdfead795df284b0ce9c7
Aung Htet Paing <aunghtetpaing@hotmail.com>
2020-06-05 09:32:10 +0630

    thk

--- Thihakyaw/src/main/java/com/ace/testing/MvcConfiguration.java
+++ Thihakyaw/src/main/java/com/ace/testing/MvcConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan
+//---
 public class MvcConfiguration extends WebMvcConfigurerAdapter
 {
     @Override
